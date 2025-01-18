6f45449daea7ef4aae163ba8b6926a5654ff10eb
Crisonel <150515636+Crisonel@users.noreply.github.com>
2025-01-18 10:33:45 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,13 +7,14 @@ dotenv.config({
     path: './.env'
 });
 
-scheduleReminder();
 
 connectDB()
 .then(() => {
     let port = process.env.PORT || 4000;
     app.listen(port, () => {
         console.log(`Server is running at port ${port}`);
+
+        scheduleReminder();
     });
 })
 .catch((err) => {
